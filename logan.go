9ae30c95972422bc9edfd70d35c9660d2d37201a
Hayden Higginbotham <hayden@higg.me>
2021-08-01 23:45:02 -0400

    Reorganize + add JSON marshaling/unmarshaling

--- logan.go
+++ logan.go
@@ -3,12 +3,13 @@ package logan
 import (
 	"math/rand"
 	"time"
+	"encoding/json"
 )
 
 type Model struct {
-	Weights []float64
-	Bias	float64
-	eta		float64
+	Weights []float64  `json:"weights"`
+	Bias	float64    `json:"bias"`
+	Eta		float64    `json:"learningRate"`
 	rand	*rand.Rand
 }
 
@@ -16,120 +17,22 @@ func New(learningRate float64) *Model {
 	return &Model {
 		Weights: []float64 {},
 		Bias: 0,
-		eta: learningRate,
+		Eta: learningRate,
 		rand: rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
 }
 
-func (mdl *Model) TrainBatch(inputs [][]float64, outputs []float64, epochs int) {
-	n := len(inputs[0])
-
-	if len(mdl.Weights) == 0 {
-		mdl.Weights = make([]float64, n)
-	}
-
-	batchEta := mdl.eta / float64(len(inputs))
-	gradient := struct { weights []float64; bias float64 } {
-		weights: make([]float64, n),
-		bias: 0,
-	}
-
-	var delta float64
-
-	for epoch := 0; epoch < epochs; epoch++ {
-		for i, input := range inputs {
-			delta = mdl.Predict(input) - outputs[i]
-
-			for j, feature := range input {
-				gradient.weights[j] -= delta * feature
-			}
-
-			gradient.bias -= delta
-		}
-
-		for j, partial := range gradient.weights {
-			mdl.Weights[j] += batchEta * partial
-			gradient.weights[j] = 0
-		}
-
-		mdl.Bias += batchEta * gradient.bias
-		gradient.bias = 0
-	}
+func Marshal(mdl *Model) ([]byte, error) {
+	return json.Marshal(mdl)
 }
 
-func (mdl *Model) TrainMiniBatch(inputs [][]float64, outputs []float64, epochs, batchSize int) {
-	m := len(inputs)
+func Unmarshal(data []byte) (*Model, error) {
+	var mdl Model
 
-	if batchSize >= m {
-		mdl.TrainBatch(inputs, outputs, epochs)
-		return
-	} else if batchSize <= 1 {
-		mdl.TrainSGD(inputs, outputs, epochs)
-		return
+	if err := json.Unmarshal(data, &mdl); err == nil {
+		mdl.rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+		return &mdl, nil
+	} else {
+		return nil, err
 	}
-
-	n := len(inputs[0])
-
-	if len(mdl.Weights) == 0 {
-		mdl.Weights = make([]float64, n)
-	}
-
-	batchEta := mdl.eta / float64(batchSize)
-	gradient := struct { weights []float64; bias float64 } {
-		weights: make([]float64, n),
-		bias: 0,
-	}
-
-	var index int
-	var delta float64
-
-	for epoch := 0; epoch < epochs; epoch++ {
-		for i := 0; i < batchSize; i++ {
-			index = mdl.rand.Intn(m)
-			delta = mdl.Predict(inputs[index]) - outputs[index]
-
-			for j, feature := range inputs[index] {
-				gradient.weights[j] -= delta * feature
-			}
-
-			gradient.bias -= delta
-		}
-
-		for j, partial := range gradient.weights {
-			mdl.Weights[j] += batchEta * partial
-			gradient.weights[j] = 0
-		}
-
-		mdl.Bias += batchEta * gradient.bias
-		gradient.bias = 0
-	}
-}
-
-func (mdl *Model) TrainSGD(inputs [][]float64, outputs []float64, epochs int) {
-	m := len(inputs)
-
-	if len(mdl.Weights) == 0 {
-		mdl.Weights = make([]float64, len(inputs[0]))
-	}
-
-	var index int
-
-	for epoch := 0; epoch < epochs; epoch++ {
-		index = mdl.rand.Intn(m)
-		mdl.Train(inputs[index], outputs[index])
-	}
-}
-
-func (mdl *Model) Train(input []float64, output float64) {
-	delta := mdl.eta * (mdl.Predict(input) - output)
-
-	for j, feature := range input {
-		mdl.Weights[j] -= delta * feature
-	}
-
-	mdl.Bias -= delta
-}
-
-func (mdl *Model) Predict(input []float64) float64 {
-	return sigmoid(dot(input, mdl.Weights) + mdl.Bias)
 }
---
+++ model.go
@@ -0,0 +1,112 @@
+func (mdl *Model) TrainBatch(inputs [][]float64, outputs []float64, epochs int) {
+	n := len(inputs[0])
+
+	if len(mdl.Weights) == 0 {
+		mdl.Weights = make([]float64, n)
+	}
+
+	batchEta := mdl.Eta / float64(len(inputs))
+	gradient := struct { weights []float64; bias float64 } {
+		weights: make([]float64, n),
+		bias: 0,
+	}
+
+	var delta float64
+
+	for epoch := 0; epoch < epochs; epoch++ {
+		for i, input := range inputs {
+			delta = mdl.Predict(input) - outputs[i]
+
+			for j, feature := range input {
+				gradient.weights[j] -= delta * feature
+			}
+
+			gradient.bias -= delta
+		}
+
+		for j, partial := range gradient.weights {
+			mdl.Weights[j] += batchEta * partial
+			gradient.weights[j] = 0
+		}
+
+		mdl.Bias += batchEta * gradient.bias
+		gradient.bias = 0
+	}
+}
+
+func (mdl *Model) TrainMiniBatch(inputs [][]float64, outputs []float64, epochs, batchSize int) {
+	m := len(inputs)
+
+	if batchSize >= m {
+		mdl.TrainBatch(inputs, outputs, epochs)
+		return
+	} else if batchSize <= 1 {
+		mdl.TrainSGD(inputs, outputs, epochs)
+		return
+	}
+
+	n := len(inputs[0])
+
+	if len(mdl.Weights) == 0 {
+		mdl.Weights = make([]float64, n)
+	}
+
+	batchEta := mdl.Eta / float64(batchSize)
+	gradient := struct { weights []float64; bias float64 } {
+		weights: make([]float64, n),
+		bias: 0,
+	}
+
+	var index int
+	var delta float64
+
+	for epoch := 0; epoch < epochs; epoch++ {
+		for i := 0; i < batchSize; i++ {
+			index = mdl.rand.Intn(m)
+			delta = mdl.Predict(inputs[index]) - outputs[index]
+
+			for j, feature := range inputs[index] {
+				gradient.weights[j] -= delta * feature
+			}
+
+			gradient.bias -= delta
+		}
+
+		for j, partial := range gradient.weights {
+			mdl.Weights[j] += batchEta * partial
+			gradient.weights[j] = 0
+		}
+
+		mdl.Bias += batchEta * gradient.bias
+		gradient.bias = 0
+	}
+}
+
+func (mdl *Model) TrainSGD(inputs [][]float64, outputs []float64, epochs int) {
+	m := len(inputs)
+
+	if len(mdl.Weights) == 0 {
+		mdl.Weights = make([]float64, len(inputs[0]))
+	}
+
+	var index int
+
+	for epoch := 0; epoch < epochs; epoch++ {
+		index = mdl.rand.Intn(m)
+		mdl.Train(inputs[index], outputs[index])
+	}
+}
+
+func (mdl *Model) Train(input []float64, output float64) {
+	delta := mdl.Eta * (mdl.Predict(input) - output)
+
+	for j, feature := range input {
+		mdl.Weights[j] -= delta * feature
+	}
+
+	mdl.Bias -= delta
+}
+
+func (mdl *Model) Predict(input []float64) float64 {
+	return sigmoid(dot(input, mdl.Weights) + mdl.Bias)
+}
